196cbe6cfba0f02e95c1cbb10baf788b9d6afe27
kawasaki <r.kawasaki.biz@gmail.com>
2022-08-10 07:19:46 +0900

    画像を入れることはできた。あとはうP

--- lib/routes/notice_route.dart
+++ lib/routes/notice_route.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Profile extends StatefulWidget {
 
@@ -8,6 +10,23 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+
+  File? profIcon;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    //カメラロールから読み取る
+    //final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        profIcon = File(pickedFile.path);
+      } else {
+        print('画像が選択できませんでした。');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +35,25 @@ class _ProfileState extends State<Profile> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(''),
+            ClipOval(
+              child:
+                  profIcon == null
+                    ? const Text('senntakuNot')
+                     : Image.file(
+                        profIcon!,
+                        width: 200,
+                        height: 200,
+                        fit: BoxFit.fill,
+                      ),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: getImage,
+                child: Text('画像を選択する'),
+              ),
+            )
           ],
         ),
       ),
